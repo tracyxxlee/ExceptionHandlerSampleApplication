c87c8d7d02c57289aacfbcc36cb42d1642389ea2
tracy <tracylee@wondercore.com>
2021-04-28 16:37:17 +0800

    Add project files.

---
+++ WebApplication/ExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ExceptionMiddleware] Exception: {ex}");
+            }
+        }
+    }
+}
